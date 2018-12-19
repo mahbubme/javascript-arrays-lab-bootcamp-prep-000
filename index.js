77314442216837b64fcd2923b85896b1e4415ac7
Mahbubur Rahman <mail@mahbub.me>
2018-12-19 15:39:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name) {
-  var new_array = kittens.push(name);
+  var new_array = kittens.concat(name);
   return new_array;
 }
